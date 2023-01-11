4c39f190bd10d01bd270a2e8b08fd8a6291dc75f
User <user@Users-MacBook-Pro.local>
2023-01-11 18:18:06 +0300

    ToDo

--- main.js
+++ main.js
@@ -582,3 +582,44 @@ element.remove();
 // const title = 'Text title';
 // const htmlExample = `<h2>${title}</h2>`;
 // container.insertAdjacentHTML('beforeend', htmlExample);
+
+
+//============ToDo список задач===================================
+
+// const todoList = document.querySelector('#todo-list');
+// const todoForm = document.querySelector('#todo-form');
+// const todoInput = document.querySelector('#todo-input');
+
+// todoForm.addEventListener('submit', formHandler);
+
+// function formHandler(event) {
+//   event.preventDefault();
+
+//   const taskText = todoInput.value;
+
+  // Создаем li через разметку
+  // const li = `<li>${taskText}</li>`;
+  // todoList.insertAdjacentHTML('beforeend', li);
+
+  // Создаем li с помощью создания элемента;
+
+//   const newTask = document.createElement('li');
+//   newTask.innerText = taskText;
+
+//   const deleteBtn = document.createElement('button');
+//   deleteBtn.setAttribute('role', 'button');
+//   deleteBtn.innerText = 'Удалить';
+//   deleteBtn.style['margin-left'] = '15px';
+//   newTask.append(deleteBtn);
+
+//   deleteBtn.addEventListener('click', function() {
+//     this.closest('li').remove();
+//   });
+
+
+//   todoList.append(newTask);
+
+//   todoInput.value = '';
+
+//   todoInput.focus();
+// };
